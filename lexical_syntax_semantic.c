ae8d23806889fb16829a224eedf8df46a990a270
YUKTA RAJENDRA MORE <114215852+Yukta2004@users.noreply.github.com>
2025-05-01 18:41:25 +0530

    Create lexical_syntax_semantic.c

---
+++ lexical_syntax_semantic.c
@@ -0,0 +1,71 @@
+// 1 lexical error
+// #include <stdio.h>
+
+// int main()
+// {
+//     printf("Compiler Design);
+//     return 0;
+// }
+
+
+//2 lexical error
+
+// #include <stdio.h>
+
+// int main(){
+// int 1age = 40;
+
+//     printf("Age:%d\n",age);
+//     return 0;
+// }
+
+
+
+
+// 1 semantic error
+// #include <stdio.h>
+// int main()
+// {
+// if (a == 10)
+// {
+//     printf("a :%d\n",a);
+// }
+
+//     return 0;
+// }
+
+
+// 2 semantic error
+
+// #include <stdio.h>
+// int main()
+// {
+//     int age = 40;
+//     {
+//         printf("Result :%d\n",result);
+//     }
+    
+//     return 0;
+// }
+
+
+
+// 1 syntax error
+// #include <stdio.h>
+
+// int main() {
+//     for (int i = 1; i <= 10; i++) {
+//         printf("%d\n" i);
+    
+//     return 0;
+// }
+
+
+//2  syntax error
+#include <stdio.h>
+
+int main()
+{
+    printf("Compiler Design)
+    return 0;
+}
